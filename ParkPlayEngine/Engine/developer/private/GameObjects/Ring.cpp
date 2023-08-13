222e3c5b4e53f5df7d4f517bdc0bdfbe7c2aacbe
dylanmiller9092 <113951578+dylanmiller9092@users.noreply.github.com>
2023-08-13 12:27:31 +1000

    tried to add transforms in the game object

--- ParkPlayEngine/Engine/developer/private/GameObjects/Ring.cpp
+++ ParkPlayEngine/Engine/developer/private/GameObjects/Ring.cpp
@@ -4,4 +4,6 @@ void Ring::BeginPlay()
 {
 	AddModel("Engine/developer/models/ring_gltf/scene.gltf");
 	AddCollider(glm::vec3(1.0f));
+	Transform.Scale *= 0.1;
+	Transform.Rotation = glm::vec3(90.0f, 0.0f, 0.0f);
 }
